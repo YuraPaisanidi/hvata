4c4f947cc7f6d1bb6b7efda47eedfbf4190fb958
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-09-21 16:26:39 +0300

    first commit

---
+++ app/js/slider.js
@@ -0,0 +1,7 @@
+// ----------------------SLIDER-hero----------------------
+var swiper = new Swiper(".proposal__slider", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
